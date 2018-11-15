a887886f444d90c3f155d2885fc20e630ecbcad3
Constantin Pojoga <constantin.pojoga@designory.com>
2018-11-15 12:49:56 -0600

    heroku test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            hi there
+            hi there on heroku
           </p>
         </header>
       </div>
