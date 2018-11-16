ff673a78ae1fbbad01060531a3aa5c674f311bca
Constantin Pojoga <constantin.pojoga@designory.com>
2018-11-15 20:23:12 -0600

    heroku test

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -1,7 +1,7 @@
 import React, { Component}  from 'react';
 import TopNav from 'components/containers/TopNav';
 import logo from './marvel-logo.png';
-import "./header.scss";
+// import "./header.scss";
 
 export default class Header extends Component {
 
