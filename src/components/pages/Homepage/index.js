44b1fff65974b86825950e2fc68f8cfa70313396
Constantin Pojoga <constantin.pojoga@designory.com>
2018-11-16 08:10:13 -0600

    Homepage styles

--- src/components/pages/Homepage/index.js
+++ src/components/pages/Homepage/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import renderHTML from 'react-render-html';
 // Components
 import GoToQuiz from 'components/presentational/GoToQuiz';
-// Model
+// Files
 import quiz from 'model/quiz.json';
+import './homepage.scss';
 
 const Homepage = () => (
     <div className="homepage">
         <div className="container">
             <h1 className="homepage__title">{quiz.sortTitle}</h1>
 
-            <p className="homepage__content">{renderHTML(quiz.introduction)}</p>
+            <p className="homepage__text">{renderHTML(quiz.introduction)}</p>
 
             <GoToQuiz href="/quiz"/>
         </div>
